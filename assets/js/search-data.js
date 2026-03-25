eadbb16377056ebe37801e9082ae95b4a069d484
supersai007 <supersai.007@gmail.com>
2026-03-25 22:11:57 +0000

    Deploying to gh-pages from @ supersai007/supersai007.github.io@69da20e1de592d12b2def4cbe3eb1bec2531dc73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My complete Curriculum Vitae. Download the PDF version below.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -501,21 +501,28 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%61%69%72%61%6D.%6B%61%73%61%6E%61%67%6F%74%74%75@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/supersai007", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/sai-ram-kasanagottu-8617562a", "_blank");
         },
       },{
         id: 'social-rss',
@@ -529,7 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=QSvIGQsAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
