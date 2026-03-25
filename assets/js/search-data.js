6bc5b48e3bc7420018fb3c3f148f660569b321cb
supersai007 <supersai.007@gmail.com>
2026-03-25 22:24:59 +0000

    Deploying to gh-pages from @ supersai007/supersai007.github.io@79d794b8b6a1ce61fc3dd17f33b97473e15ae2e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "A record of my teaching assistantships and instructional responsibilities.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
